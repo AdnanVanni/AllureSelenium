acd3de994f173d90d84f3ce6802c273051b31cd6
Adnan <adnanvanni@gmail.com>
2024-05-17 17:20:13 +0530

    issue

---
+++ src/test/java/simple/MyTest.java
@@ -0,0 +1,35 @@
+package simple;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+public class MyTest {
+	 @Test
+	    public void testSearch()
+	    {
+	        WebDriver D= new ChromeDriver();
+	        D.get("https://www.google.com");
+	        D.findElement(By.xpath("//*[@class='gLFyf']")).sendKeys("testing google website");
+	        D.findElement(By.xpath("//*[@class='gLFyf']")).sendKeys(Keys.ENTER);
+	        Assert.assertEquals(D.getTitle(),"testing google website - Google Search");
+	}
+	    @Test
+	    public void testSearchfaliling()
+	    {
+	        WebDriver D= new ChromeDriver();
+	        D.get("https://www.google.com");
+	        D.findElement(By.xpath("//*[@class='gLFyf']")).sendKeys("testing google website");
+	        D.findElement(By.xpath("//*[@class='gLFyf']")).sendKeys(Keys.ENTER);
+	       // Assert.assertEquals(D.getTitle(),"testing1 google website - Google Search");
+	    }
+}
+
+
+
